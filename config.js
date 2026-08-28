3b318f6d464eef3e18f3744241a81c9d27759ecb
diodibrugarh1-cell <dio.dibrugarh1@gmail.com>
2026-08-28 14:05:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-const SUPABASE_URL = "https://tpzdgmbnyvkoqmoyupnb.supabase.co";
-
-const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_kPa8pfXOp-p1V6DpAFJLnw_zpmps8C8";
-
-export { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY };
+window.APP_CONFIG = {
+    SUPABASE_URL: "https://tpzdgmbnyvkoqmoyupnb.supabase.co",
+    SUPABASE_ANON_KEY: "sb_publishable_kPa8pfXOp-p1V6DpAFJLnw_zpmps8C8"
+};
