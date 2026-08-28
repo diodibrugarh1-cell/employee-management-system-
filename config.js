849eecf61dc4abb65a43b4c1460e348c71f55627
diodibrugarh1-cell <dio.dibrugarh1@gmail.com>
2026-08-28 14:48:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.APP_CONFIG = {
     SUPABASE_URL: "https://tpzdgmbnyvkoqmoyupnb.supabase.co",
-    SUPABASE_ANON_KEY: "sb_publishable_kPa8pfXOp-p1V6DpAFJLnw_zpmps8C8};
+    SUPABASE_ANON_KEY: "sb_publishable_kPa8pfXOp-p1V6DpAFJLnw_zpmps8C8"
+};
